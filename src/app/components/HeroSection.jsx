d2818a52c1e5c679f73cbc7e41f696c27ef30d6d
Ahmed Gharbi <ahmedgharbigharbi718@gmail.com>
2024-11-17 03:16:02 +0100

    add priority to hero image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -63,6 +63,7 @@ const HeroSection = () => {
             <Image
               src="/images/hero-image.png"
               alt="hero image"
+              priority={true}
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={400}
               height={400}
