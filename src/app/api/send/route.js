9beb37d093926ec23ac15d3e6658b9be319a9dc7
Ahmed Gharbi <ahmedgharbigharbi718@gmail.com>
2025-03-12 02:04:44 +0100

    delete playground

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -12,27 +12,4 @@ export async function POST(request) {
       }
     );
   }
-
-  try {
-    const response = await emailjs.send(
-      'service_c3g1so7',
-      'template_12345',
-      { user_email: email, subject, message },
-      'eStu4Qmry3z_Mtbr8LFht'
-    );
-
-    return new Response(
-      JSON.stringify({ message: 'Email sent successfully!', response }),
-      {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  } catch (error) {
-    console.error('Error sending email:', error);
-    return new Response(JSON.stringify({ error: 'Failed to send email' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
 }
